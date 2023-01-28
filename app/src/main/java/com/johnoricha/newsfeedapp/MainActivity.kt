beb0580ccd776764de6f855d3d1ee4b7170b9036
johnoricha <johnrichman664@gmail.com>
2023-01-28 04:02:30 +0100

    remove unused imports

--- app/src/main/java/com/johnoricha/newsfeedapp/MainActivity.kt
+++ app/src/main/java/com/johnoricha/newsfeedapp/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.johnoricha.newsfeedapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.johnoricha.newsfeedapp.ui.components.NewsfeedHomeScreen
 import com.johnoricha.newsfeedapp.ui.theme.NewsfeedAppTheme
 import com.johnoricha.newsfeedapp.ui.viewmodels.NewsfeedViewModel
